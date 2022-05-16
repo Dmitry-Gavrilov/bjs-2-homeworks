85fa5c7f7067badac6ec36c2199d614a3bf3f631
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-05-16 21:52:44 +1000

    zadanie_1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+
+  "use strict";
+  let d = Math.pow(b, 2) - 4*a*c;
+  
+  
+  if (d < 0) {
+    arr = [];
+  } 
+  
+  if (d === 0) {
+    arr = [(-b / (2 * a))];
+    
+   
+  } 
+  if (d > 0) {
+
+    arr = [((-b + Math.sqrt(d) ) / (2 * a)) , ((-b - Math.sqrt(d) ) / (2 * a))];
+    
+  }
+
+
   return arr; // array
-}
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+ 
+
 
   // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
