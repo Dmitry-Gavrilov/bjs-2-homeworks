ade787ee9e7a16a674e5f930c221da643510c48d
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-05-23 22:01:17 +1000

    2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((item,i) => item === arr2[i]);
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr.filter((item) => item > 0)
+  .filter((item) => item % 3 === 0)
+  .map((item) => item * 10);
+ 
   return resultArr; // array
 }
